8dd8585b3ffb0d270bf86bcc64f06b6a0106a110
wtfloltk <153698146+wtfloltk@users.noreply.github.com>
2023-12-25 02:51:37 -0500

    Create libfatversion.h

---
+++ include/libfatversion.h
@@ -0,0 +1,18 @@
+/**
+taken from that one place i got it from i dno tbh....
+like what
+obv i didnt make the damn libfat thing
+peace out
+brendan
+ **/
+
+#ifndef __LIBFATVERSION_H__
+#define __LIBFATVERSION_H__
+
+#define _LIBFAT_MAJOR_	1
+#define _LIBFAT_MINOR_	0
+#define _LIBFAT_PATCH_	11
+
+#define _LIBFAT_STRING "libFAT"
+
+#endif // __LIBFATVERSION_H__
